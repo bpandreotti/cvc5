9e26417b50b92596ac8988e6ee2dcca190060b87
Bruno Andreotti <bandreotti@proton.me>
2025-09-03 16:12:47 -0300

    simplify `estimateTreeSize`

--- src/theory/uf/equality_engine.h
+++ src/theory/uf/equality_engine.h
@@ -672,7 +672,7 @@ class EqualityEngine : public context::ContextNotifyObj, protected EnvObj
 
   void computeGreedyWeights();
 
-  int estimateTreeSize(EqualityNodeId start, EqualityNodeId end, uint32_t maxLevel);
+  int estimateTreeSize(EqualityNodeId start, EqualityNodeId end);
 
   void computeExtraRedundantEdges();
 
