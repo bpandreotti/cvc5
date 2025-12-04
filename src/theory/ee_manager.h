f246390067aa748570905d4018ed0179003e54b2
Bruno Andreotti <bandreotti@proton.me>
2025-12-04 16:51:46 -0300

    Make `ModelManager` use central equality engine

--- src/theory/ee_manager.h
+++ src/theory/ee_manager.h
@@ -74,6 +74,8 @@ class EqEngineManager : protected EnvObj
    */
   const EeTheoryInfo* getEeTheoryInfo(TheoryId tid) const;
 
+  eq::EqualityEngine* getCentralEqualityEngine();
+
   /** Allocate equality engine that is context-dependent on c with info esi */
   eq::EqualityEngine* allocateEqualityEngine(EeSetupInfo& esi,
                                              context::Context* c);
